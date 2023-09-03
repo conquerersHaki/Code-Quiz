62160a5388d11c21133bce36199dbac923c8067d
unknown <keighleychristina@yahoo.com>
2023-09-02 17:11:43 -0700

    completed css and JS

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+  numb: 1,
+  question: "What does HTML stand for?",
+  answer: "Hyper Text Markup Language",
+  options: [
+    "Hyper Text Preprocessor",
+    "Hyper Text Markup Language",
+    "Hyper Text Multiple Language",
+    "Hyper Tool Multi Language"
+  ]
+},
+  {
+  numb: 2,
+  question: "What are the CSS properties that are used to add space around sections of content?",
+  answer: "Padding",
+  options: [
+    "Spacing",
+    "Break",
+    "Cleaner",
+    "Padding"
+  ]
+},
+  {
+  numb: 3,
+  question: "What does CSS stand for?",
+  answer: "Cascading Style Sheet",
+  options: [
+    "Common Style Sheet",
+    "Colorful Style Sheet",
+    "Computer Style Sheet",
+    "Cascading Style Sheet"
+  ]
+},
+  {
+  numb: 4,
+  question: "What is the CSS property that offers extra information about something when you hover over an element?",
+  answer: "Tooltip",
+  options: [
+    "Tooltip",
+    "Hint",
+    "Info Block",
+    "Tutorial"
+  ]
+},
+  {
+  numb: 5,
+  question: "What is the name of the statement that is used to exit or end a loop?",
+  answer: "Break statement",
+  options: [
+    "Break statement",
+    "Close statement",
+    "Falter statement",
+    "Conditional statement"
+  ]
+}
+]
